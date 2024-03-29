754099e372501af4ce86bde3fcde4446584f4846
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2024-03-29 09:30:59 -1200

    Add files via upload

---
+++ assignment-9/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favoriteNumber = 5;
+let message = `My favourite number is ${favoriteNumber} because it is my date of birth.`;
+console.log(message);
---
+++ assignment-9/main.ts
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favoriteNumber:number = 5;
+let message:string = `My favourite number is ${favoriteNumber} because it is my date of birth.`
+console.log(message);
