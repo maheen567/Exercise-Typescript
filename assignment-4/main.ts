dcf3741b9c7386ab0bbda570a9204765452cd2d6
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2024-03-28 16:10:23 -1200

    Add files via upload

---
+++ assignment-4/main.ts
@@ -0,0 +1,6 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let famousPerson = "Steve Jobs";
+console.log('Steve Jobs once said, "Have the courage to follow your heart and intuition.They somehow know what you truly want to become."');
+
