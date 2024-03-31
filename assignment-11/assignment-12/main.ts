126e14e9aac97fd115cfe7d908ab7da834b0d8b8
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2024-03-31 08:54:29 -1200

    Add files via upload

--- assignment-11/assignment-12/main.ts
+++
@@ -1,9 +0,0 @@
-//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-
-
-let names:string[] =["Nabiha", "Ayesha", "Sarah", "Mahi", "Fatima"]
-const message:string = "Welcome to the Typescript class!"
-
-for(let i=0; i<names.length; i++) {                       //for loop
-     console.log(message + names[i]); 
-};
--- assignment-11/main.ts
+++
@@ -1,6 +0,0 @@
-//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let names:string[] =["Nabiha", "Ayesha", "Sarah", "Mahi", "Fatima"]
-
-for(let i=0; i<names.length; i++) {                       //for loop
-     console.log(names[i]); 
-};
