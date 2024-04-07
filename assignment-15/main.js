dec82326aa8494745c75b9fb097033a17ebe6bdf
Maheen Hussain <165302435+maheen567@users.noreply.github.com>
2024-04-07 05:28:50 -1200

    Add files via upload

---
+++ assignment-15/main.js
@@ -0,0 +1,19 @@
+"use strict";
+//Question15: Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+Object.defineProperty(exports, "__esModule", { value: true });
+//• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+//• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+//• Print a second set of invitation messages, one for each person who is still in your list.
+let guest = ["Rana", "Sana", "Mana", "Rani"];
+for (let i = 0; i < guest.length; i++) {
+    console.log(`/nYou're invited to a dinner,n/ ` + guest[i]);
+}
+;
+let absentGuest = "Rana";
+let newGuest = "Hassan";
+guest[0] = newGuest;
+for (let i = 0; i < guest.length; i++) {
+    console.log(`You're invited to a dinner, ` + guest[i]);
+}
+;
+console.log(`Mr.${absentGuest} is not coming to the party.`);
